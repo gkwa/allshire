41a28efc3a2598586fd62379dc9cae8f28c5dbc6
Taylor Monacelli <taylormonacelli@gmail.com>
2024-05-12 20:52:28 -0700

    Fix version for allshire

--- {{ cookiecutter.project_slug }}/main.go
+++ {{ cookiecutter.project_slug }}/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
+	args := os.Args[1:]
+
+	if len(args) > 0 && (args[0] == "version" || args[0] == "-version") {
+		buildInfo := GetBuildInfo()
+		fmt.Println(buildInfo)
+		os.Exit(0)
+	}
+
 	fmt.Println("Hello, World!")
 }
